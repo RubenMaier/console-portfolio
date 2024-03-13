dd2a7595824be2a75acc40508ab21d2ee69afb37
RubenMaier <ruben@rebill.to>
2024-03-12 21:55:52 -0300

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,29 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Ruben Maier Enzler",
   description: "Software Engineer",
+  referrer: "origin-when-cross-origin",
+  authors: [
+    { name: "Rub" },
+    { name: "Ruben Maier Enzler", url: "https://rub.dev" },
+  ],
+  keywords: [
+    "TypeScript",
+    "Developer",
+    "Software",
+    "Next",
+    "AWS",
+    "Nest",
+    "Engineer",
+    "React",
+    "JavaScript",
+  ],
+  openGraph: {
+    title: "Rub Portfolio",
+    description: "Software Engineer",
+    url: "https://rub.dev",
+    images: "/og-image.png",
+    siteName: "Ruben",
+  },
 };
 
 export default function RootLayout({
